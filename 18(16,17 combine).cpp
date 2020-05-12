24da4225437e1cc92ed8570e9ed5d94b470bf4a4
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-05-12 20:30:29 +0600

    Add files via upload

---
+++ 18(16,17 combine).cpp
@@ -0,0 +1,42 @@
+#include<iostream>>
+using namespace std;
+int main()
+{
+   int row,col,n;
+   cout<<"Enter size:";
+   cin>>n;
+   for(row=1;row<=n;row++)
+   {
+       for(col=1;col<=n-row;col++)
+       {
+           cout<<" ";
+       }
+       for(col=1;col<=row;col++)
+       {
+           cout<<col ;
+       }
+       for(col=row-1;col>=1;col--)
+       {
+           cout<<col;
+       }
+       cout<<endl;
+   }
+   for(row=n-1;row>=1;row--)
+   {
+       for(col=1;col<=n-row;col++)
+       {
+           cout<<" ";
+       }
+       for(col=1;col<=row;col++)
+       {
+           cout<<col ;
+       }
+       for(col=row-1;col>=1;col--)
+       {
+           cout<<col;
+       }
+       cout<<endl;
+   }
+}
+
+

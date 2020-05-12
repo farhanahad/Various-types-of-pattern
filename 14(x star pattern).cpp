899ae17ddd015c304278738a47accff9a3ff93ef
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-05-12 20:20:17 +0600

    Add files via upload

---
+++ 14(x star pattern).cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,col,n;
+    cout<<"Enter size:";
+    cin>>n;
+    for(row=1;row<=n;row++)
+    {
+        for(col=1;col<=n;col++)
+        {
+            if(row==col || row+col==n+1)
+                cout<<"*";
+            else
+                cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
